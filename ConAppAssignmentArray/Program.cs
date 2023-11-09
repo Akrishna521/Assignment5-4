d970cd08fec1ec2739688870e1f3ce4b5dd7764d
Akrishna521 <anishetty.saikrishna@mphasis.com>
2023-11-09 15:16:44 +0530

    Assignment5-4

---
+++ ConAppAssignmentArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        int[] numbers = new int[5];
+        Random random = new Random();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 101);
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        double average = (double)sum / numbers.Length;
+
+        Console.WriteLine("Sum of the elements in the array: " + sum);
+        Console.WriteLine("Average of the elements in the array: " + average);
+    }
+}
